202320601cbd76c99fc60451777d0bcd3da8edd7
Nick <nickychirico@mac.com>
2019-02-27 14:51:07 -0500

    AGHJ

--- Light Prototype/Assets/Scripts/SourceScript.cs
+++ Light Prototype/Assets/Scripts/SourceScript.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class SourceScript : MonoBehaviour
 {
+    public GameObject visionMask;
+    private CircleCollider2D coll;
+    
     public float lightStored;
+
+    private void Start()
+    {
+        coll = this.GetComponent<CircleCollider2D>();
+        visionMask.transform.localScale = new Vector3(2, 2, 1);
+    }
+
 }
