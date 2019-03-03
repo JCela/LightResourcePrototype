4c0c69012e09b75415ba57059a9e0206cb9e5499
Nick <nickychirico@mac.com>
2019-03-03 16:45:22 -0500

    xx

---
+++ Light Prototype/Assets/Scripts/Flare.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flare : MonoBehaviour
+{
+    //public float stallTime
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
